027a35ee8d8675ca864190a9d9ca4fefbf2686ac
Sven Ruppert <sven.ruppert@gmail.com>
2017-07-21 15:48:13 +1200

    v002

---
+++ src/main/java/org/rapidpm/frp/v002/MathService.java
@@ -0,0 +1,24 @@
+package org.rapidpm.frp.v002;
+
+/**
+ *
+ */
+public class MathService {
+  public MathTask nextMathTask() {
+    int first = 0;
+    int second = 0;
+    int result = 0;
+    // create task...
+
+    return new MathTask(first , second , (first + second));
+  }
+
+
+  public boolean validate(MathTaskResult mathTaskResult){
+    return mathTaskResult.getHumanResult() ==
+           mathTaskResult.getMathTask().getResult();
+  }
+
+
+
+}
